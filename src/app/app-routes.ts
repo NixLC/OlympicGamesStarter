2614beacdf2509860ef2fbd786a84098fa9a8b70
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-06-27 14:08:11 +0200

    needed project file

---
+++ src/app/app-routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {HomeComponent} from "./pages/home/home.component";
+import {CountryDetailComponent} from "./pages/country-detail/country-detail.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'country/:country',
+    component: CountryDetailComponent
+  },
+  {
+    path: '**', // wildcard
+    component: NotFoundComponent,
+  }
+];
