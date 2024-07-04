8ecf47358564462efcaf9823664c7ac8243bb8c6
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-07-04 04:10:48 +0200

    production mode is set during build time

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import {enableProdMode} from '@angular/core';
 
 import {appConfig} from './app/app.config';
-import {environment} from './environments/environment.prod';
+import {environment} from './environments/environment';
 import {AppComponent} from "./app/app.component";
 import {bootstrapApplication} from "@angular/platform-browser";
 
